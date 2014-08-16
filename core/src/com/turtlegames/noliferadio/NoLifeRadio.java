b285440f246d47a32ad83ad067a2a171ec6285f4
Sebastián <scanevaro@hotmail.com>
2014-08-16 18:20:25 -0300

    woerk

--- core/src/com/turtlegames/noliferadio/NoLifeRadio.java
+++ core/src/com/turtlegames/noliferadio/NoLifeRadio.java
@@ -4,43 +4,82 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
-public class NoLifeRadio extends ApplicationAdapter
-{
-	private SpriteBatch spriteBatch;
-	private Stage stage;
-	private ImageButton buttonPlay;
-	private Skin skin;
+public class NoLifeRadio extends ApplicationAdapter implements Runnable {
+    private SpriteBatch spriteBatch;
+    private Stage stage;
+    private TextButton buttonPlay;
+    private Skin skin;
+    private MediaPlayer mediaPlayer;
+    private MediaPlayerThread mediaPlayerThread;
 
-	@Override
-	public void create()
-	{
-		spriteBatch = new SpriteBatch();
-		stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
-		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+    @Override
+    public void create() {
+        spriteBatch = new SpriteBatch();
+        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
-		//UITest on LibGDX Tests
-		buttonPlay = new ImageButton((Drawable) Gdx.files.internal("data/play.png"));
-		//		buttonPlay.setWidth(64f);
-		//		buttonPlay.setHeight(64f);
-		buttonPlay.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        //TODO Check UITest on LibGDX Tests for button with Image
+        buttonPlay = new TextButton("Play", skin, "default");
+        //		buttonPlay.setWidth(64f);
+        //		buttonPlay.setHeight(64f);
+        buttonPlay.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        buttonPlay.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if (mediaPlayerThread == null) {
+                    mediaPlayerThread = new MediaPlayerThread();
+                    new Thread(mediaPlayerThread).start();
+                }
+            }
+        });
 
-		stage.addActor(buttonPlay);
-	}
+        stage.addActor(buttonPlay);
 
-	@Override
-	public void render()
-	{
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.input.setInputProcessor(stage);
+    }
 
-		spriteBatch.begin();
-		stage.draw();
-		spriteBatch.end();
-	}
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    @Override
+    public void run() {
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer(new Media("http://radio.nolife-radio.com:8000"));
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (mediaPlayer.isPlaying())
+            mediaPlayer.stop();
+    }
+
+    @Override
+    public void dispose() {
+        runnable.stop();
+    }
+
+    private class MediaPlayerThread implements Runnable {
+        @Override
+        public void run() {
+            if (mediaPlayer == null)
+                mediaPlayer.play();
+        }
+    }
 }
