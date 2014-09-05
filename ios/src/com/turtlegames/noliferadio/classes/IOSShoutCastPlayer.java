0100a43606aa6d9c77898c91c6c905a48a7483be
Sebastián <scanevaro@hotmail.com>
2014-09-04 21:47:43 -0300

    work on player states on iOS

--- ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
+++ ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
@@ -1,7 +1,10 @@
 package com.turtlegames.noliferadio.classes;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.turtlegames.noliferadio.interfaces.Player;
 import org.robovm.apple.foundation.NSURL;
+import org.robovm.apple.mediaplayer.MPMoviePlaybackState;
 import org.robovm.apple.mediaplayer.MPMoviePlayerController;
 import org.robovm.apple.mediaplayer.MPMoviePlayerViewController;
 import org.robovm.apple.mediaplayer.MPMovieSourceType;
@@ -9,24 +12,36 @@ import org.robovm.apple.mediaplayer.MPMovieSourceType;
 /**
  * Created by scanevaro on 23/08/2014.
  */
-public class IOSShoutCastPlayer implements Player
-{
-	private MPMoviePlayerController player;
+public class IOSShoutCastPlayer implements Player, Runnable {
+    private MPMoviePlayerController player;
+    private MPMoviePlaybackState listener;
+    private Label status;
 
-	public IOSShoutCastPlayer()
-	{
-		player = new MPMoviePlayerViewController().getMoviePlayer();
-	}
+    public IOSShoutCastPlayer() {
+        player = new MPMoviePlayerViewController().getMoviePlayer();
+        listener = player.getPlaybackState();
+    }
 
-	@Override
-	public void play(String url)
-	{
-		player.setMovieSourceType(MPMovieSourceType.Streaming);
+    @Override
+    public void play(String url, Label status) {
+        this.status = status;
 
-		player.setContentURL(new NSURL(url));
+        player.setMovieSourceType(MPMovieSourceType.Streaming);
+
+        player.setContentURL(new NSURL(url));
 
         player.setShouldAutoplay(true);
 
-		player.play();
-	}
+        player.play();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (listener.value() == MPMoviePlaybackState.Playing.ordinal()) {
+                status.setText("Playing");
+                status.setColor(Color.GREEN);
+            }
+        }
+    }
 }
