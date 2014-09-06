9893dc4cdfe2dde5439708118b21db66e257c3e5
Sebastián <scanevaro@hotmail.com>
2014-09-05 21:10:18 -0300

    work on player states on iOS

--- core/src/com/turtlegames/noliferadio/NoLifeRadio.java
+++ core/src/com/turtlegames/noliferadio/NoLifeRadio.java
@@ -15,162 +15,152 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.turtlegames.noliferadio.interfaces.Player;
 
-public class NoLifeRadio extends ApplicationAdapter
-{
-	private SpriteBatch spriteBatch;
-	private Stage stage;
-	private ImageButton buttonPlay;
-	private Skin skin;
-	private Player player;
-	private Label state;
-	private Table table;
+public class NoLifeRadio extends ApplicationAdapter {
+    private SpriteBatch spriteBatch;
+    private Stage stage;
+    private ImageButton buttonPlay;
+    private Skin skin;
+    private Player player;
+    private Label state;
+    private Table table;
 
-	private ClickListener cickListener;
+    private ClickListener cickListener;
 
-	public NoLifeRadio(Player player)
-	{
-		if (player != null)
-			this.player = player;
-	}
+    public NoLifeRadio(Player player) {
+        if (player != null)
+            this.player = player;
+    }
 
-	@Override
-	public void create()
-	{
-		spriteBatch = new SpriteBatch();
-		stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
-		skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+    @Override
+    public void create() {
+        spriteBatch = new SpriteBatch();
+        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
-		Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(stage);
 
-		table = new Table();
-		table.setFillParent(true);
-		table.pad(30);
-		stage.addActor(table);
+        table = new Table();
+        table.setFillParent(true);
+        table.pad(30);
+        stage.addActor(table);
 
-		prepareTitle();
-		preparePlayButton();
-		prepareTree();
-	}
+        prepareTitle();
+        preparePlayButton();
+        prepareTree();
+    }
 
-	@Override
-	public void render()
-	{
-		Gdx.gl.glClearColor(0, 0.1f, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0.1f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		spriteBatch.begin();
+        spriteBatch.begin();
 
-		stage.draw();
+        stage.draw();
 
-		table.debug();
+        table.debug();
 
-		spriteBatch.end();
-	}
+        spriteBatch.end();
+    }
 
-	private void prepareTitle()
-	{
-		table.add(new Image(new Texture(Gdx.files.internal("data/title1.png"))));
-		table.add(new Image(new Texture(Gdx.files.internal("data/title2.png"))));
+    private void prepareTitle() {
+        table.add(new Image(new Texture(Gdx.files.internal("data/title1.png"))));
+        table.add(new Image(new Texture(Gdx.files.internal("data/title2.png"))));
 
-		table.row();
-		table.padTop(20);
-	}
+        table.row().spaceBottom(50);
+    }
 
-	private void preparePlayButton()
-	{
-		Texture playTexture = new Texture(Gdx.files.internal("data/play.png"));
-		Texture stopTexture = new Texture(Gdx.files.internal("data/stop.png"));
+    private void preparePlayButton() {
+        Texture playTexture = new Texture(Gdx.files.internal("data/play.png"));
+        Texture stopTexture = new Texture(Gdx.files.internal("data/stop.png"));
 
-		TextureRegion playTexReg = new TextureRegion(playTexture);
-		TextureRegion stopTexReg = new TextureRegion(stopTexture);
+        TextureRegion playTexReg = new TextureRegion(playTexture);
+        TextureRegion stopTexReg = new TextureRegion(stopTexture);
 
-		ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle(skin.get(Button.ButtonStyle.class));
-		style.imageUp = new TextureRegionDrawable(playTexReg);
-		style.imageChecked = new TextureRegionDrawable(stopTexReg);
+        ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle(skin.get(Button.ButtonStyle.class));
+        style.imageUp = new TextureRegionDrawable(playTexReg);
+        style.imageChecked = new TextureRegionDrawable(stopTexReg);
 
-		buttonPlay = new ImageButton(style);
-		buttonPlay.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		buttonPlay.addListener(new ClickListener()
-		{
-			@Override
-			public void clicked(InputEvent event, float x, float y)
-			{
-				//TODO desktop play not working
-				String url = "http://radio.nolife-radio.com:9000/stream";
-				player.play(url, state);
-			}
-		});
+        buttonPlay = new ImageButton(style);
+        buttonPlay.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        buttonPlay.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //TODO desktop play not working
+                String url = "http://radio.nolife-radio.com:9000/stream";
+                player.play(url, state);
+            }
+        });
 
-		Table _table = new Table(skin);
-		_table.add(buttonPlay);
-		state = new Label("...IDLE...", skin);
-		_table.add(new Label("Status: ", skin), state);
-		table.add(_table).align(Align.left);
-		table.add();
-		table.row();
-		table.add().expand();
-	}
+        Table _table = new Table(skin);
+        _table.add(buttonPlay);
+        state = new Label("...IDLE...", skin);
+        _table.add(new Label("Status: ", skin), state);
+        table.add(_table).align(Align.left);
+        table.add();
+        table.row();
+    }
 
-	private void prepareTree()
-	{
-		final Tree tree = new Tree(skin);
+    private void prepareTree() {
+        final Tree tree = new Tree(skin);
 
-		//button now playing
-		ImageButton.ImageButtonStyle nowPlayingStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
-		nowPlayingStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/nowPlayingFolded.png"))));
-		nowPlayingStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/nowPlayingUnFolded.png")))));
-		final Tree.Node nowPlayingNode = new Tree.Node(new ImageButton(nowPlayingStyle));
-		//set listener
-		nowPlayingNode.getActor().addListener(new ClickListener()
-		{
-			public void clicked(InputEvent event, float x, float y)
-			{
-				if (!nowPlayingNode.isExpanded())
-					nowPlayingNode.setExpanded(true);
-				else
-					nowPlayingNode.setExpanded(false);
-			}
-		});
+        //button now playing
+        ImageButton.ImageButtonStyle nowPlayingStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
+        nowPlayingStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/nowPlayingFolded.png"))));
+        nowPlayingStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/nowPlayingUnFolded.png")))));
+        final Tree.Node nowPlayingNode = new Tree.Node(new ImageButton(nowPlayingStyle));
+        //set listener
+        nowPlayingNode.getActor().addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if (!nowPlayingNode.isExpanded())
+                    nowPlayingNode.setExpanded(true);
+                else
+                    nowPlayingNode.setExpanded(false);
+            }
+        });
 
-		ImageButton.ImageButtonStyle chatStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
-		chatStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/chatFolded.png"))));
-		chatStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/chatUnFolded.png")))));
-		final Tree.Node chatNode = new Tree.Node(new ImageButton(chatStyle));
+        ImageButton.ImageButtonStyle chatStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
+        chatStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/chatFolded.png"))));
+        chatStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/chatUnFolded.png")))));
+        final Tree.Node chatNode = new Tree.Node(new ImageButton(chatStyle));
 
-		chatNode.getActor().addListener(new ClickListener()
-		{
-			public void clicked(InputEvent event, float x, float y)
-			{
-				if (!chatNode.isExpanded())
-					chatNode.setExpanded(true);
-				else
-					chatNode.setExpanded(false);
-			}
-		});
+        chatNode.getActor().addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if (!chatNode.isExpanded())
+                    chatNode.setExpanded(true);
+                else
+                    chatNode.setExpanded(false);
+            }
+        });
 
-		ImageButton.ImageButtonStyle aboutStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
-		aboutStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/aboutFolded.png"))));
-		aboutStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/aboutUnFolded.png")))));
-		final Tree.Node aboutNode = new Tree.Node(new ImageButton(aboutStyle));
+        ImageButton.ImageButtonStyle aboutStyle = new ImageButton.ImageButtonStyle(/*skin.get(Button.ButtonStyle.class)*/);
+        aboutStyle.imageUp = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/aboutFolded.png"))));
+        aboutStyle.imageChecked = new TextureRegionDrawable(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/aboutUnFolded.png")))));
+        final Tree.Node aboutNode = new Tree.Node(new ImageButton(aboutStyle));
 
-		aboutNode.getActor().addListener(new ClickListener()
-		{
-			public void clicked(InputEvent event, float x, float y)
-			{
-				if (!aboutNode.isExpanded())
-					aboutNode.setExpanded(true);
-				else
-					aboutNode.setExpanded(false);
-			}
-		});
+        aboutNode.getActor().addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                if (!aboutNode.isExpanded())
+                    aboutNode.setExpanded(true);
+                else
+                    aboutNode.setExpanded(false);
+            }
+        });
 
-		final Tree.Node moo4 = new Tree.Node(new TextButton("moo4", skin));
+        final Tree.Node moo4 = new Tree.Node(new TextButton("moo4", skin));
 
-		tree.add(nowPlayingNode);
-		tree.add(chatNode);
-		tree.add(aboutNode);
-		chatNode.add(moo4);
+        tree.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
 
-		table.add(tree).fill().expand().colspan(2);
-	}
+            }
+        });
+
+        tree.add(nowPlayingNode);
+        tree.add(chatNode);
+        tree.add(aboutNode);
+        chatNode.add(moo4);
+
+        table.add(tree).fill().expand().colspan(2);
+    }
 }
--- ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
+++ ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
@@ -3,8 +3,8 @@ package com.turtlegames.noliferadio.classes;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.turtlegames.noliferadio.interfaces.Player;
+import org.robovm.apple.foundation.NSNotification;
 import org.robovm.apple.foundation.NSURL;
-import org.robovm.apple.mediaplayer.MPMoviePlaybackState;
 import org.robovm.apple.mediaplayer.MPMoviePlayerController;
 import org.robovm.apple.mediaplayer.MPMoviePlayerViewController;
 import org.robovm.apple.mediaplayer.MPMovieSourceType;
@@ -14,6 +14,7 @@ import org.robovm.apple.mediaplayer.MPMovieSourceType;
  */
 public class IOSShoutCastPlayer implements Player {
     private MPMoviePlayerController player;
+    private NSNotification observerLoadStateDidChange;
     private Label status;
 
     public IOSShoutCastPlayer() {
@@ -26,10 +27,46 @@ public class IOSShoutCastPlayer implements Player {
 
         player.setMovieSourceType(MPMovieSourceType.Streaming);
 
+//        observerLoadStateDidChange = MPMoviePlayerController.Notifications.observeLoadStateDidChange(
+//                moviePlayerController,
+//                new VoidBlock1<MPMoviePlayerController>() {
+//                    @Override
+//                    public void invoke(MPMoviePlayerController player) {
+//                        MPMovieLoadState loadState = player.getLoadState();
+//                        /* The load state is not known at this time. */
+//                        if (loadState.contains(MPMovieLoadState.Unknown)) {
+//                            overlayController.setLoadStateDisplayString("unknown");
+//                        }
+//                        /*
+//                         * The buffer has enough data that playback can begin,
+//                         * but it may run out of data before playback finishes.
+//                         */
+//                        if (loadState.contains(MPMovieLoadState.Playable)) {
+//                            overlayController.setLoadStateDisplayString("playable");
+//                        }
+//                        /*
+//                         * Enough data has been buffered for playback to
+//                         * continue uninterrupted.
+//                         */
+//                        if (loadState.contains(MPMovieLoadState.PlaythroughOK)) {
+//                            // Add an overlay view on top of the movie view
+//                            addOverlayView();
+//                            overlayController.setLoadStateDisplayString("playthrough ok");
+//                        }
+//                        /* The buffering of data has stalled. */
+//                        if (loadState.contains(MPMovieLoadState.Stalled)) {
+//                            overlayController.setLoadStateDisplayString("stalled");
+//                        }
+//                    }
+//                });
+
         player.setContentURL(new NSURL(url));
 
         player.setShouldAutoplay(true);
 
         player.play();
+
+        status.setText("Playing");
+        status.setColor(Color.GREEN);
     }
 }
