71d66d45c035669b9f2ce23d2bd91299f528b23f
Sebastián <scanevaro@hotmail.com>
2014-09-06 22:43:36 -0300

    work on player states on iOS

--- ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
+++ ios/src/com/turtlegames/noliferadio/classes/IOSShoutCastPlayer.java
@@ -3,18 +3,20 @@ package com.turtlegames.noliferadio.classes;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.turtlegames.noliferadio.interfaces.Player;
-import org.robovm.apple.foundation.NSNotification;
+import org.robovm.apple.foundation.NSObject;
 import org.robovm.apple.foundation.NSURL;
+import org.robovm.apple.mediaplayer.MPMovieLoadState;
 import org.robovm.apple.mediaplayer.MPMoviePlayerController;
 import org.robovm.apple.mediaplayer.MPMoviePlayerViewController;
 import org.robovm.apple.mediaplayer.MPMovieSourceType;
+import org.robovm.objc.block.VoidBlock1;
 
 /**
  * Created by scanevaro on 23/08/2014.
  */
 public class IOSShoutCastPlayer implements Player {
     private MPMoviePlayerController player;
-    private NSNotification observerLoadStateDidChange;
+    private NSObject observerLoadStateDidChange;
     private Label status;
 
     public IOSShoutCastPlayer() {
@@ -22,27 +24,29 @@ public class IOSShoutCastPlayer implements Player {
     }
 
     @Override
-    public void play(String url, Label status) {
+    public void play(String url, final Label status) {
         this.status = status;
 
         player.setMovieSourceType(MPMovieSourceType.Streaming);
 
         observerLoadStateDidChange = MPMoviePlayerController.Notifications.observeLoadStateDidChange(
-                moviePlayerController,
+                player,
                 new VoidBlock1<MPMoviePlayerController>() {
                     @Override
                     public void invoke(MPMoviePlayerController player) {
                         MPMovieLoadState loadState = player.getLoadState();
                         /* The load state is not known at this time. */
                         if (loadState.contains(MPMovieLoadState.Unknown)) {
-                            overlayController.setLoadStateDisplayString("unknown");
+//                            overlayController.setLoadStateDisplayString("unknown");
+                            status.setText("...IDLE...");
                         }
                         /*
                          * The buffer has enough data that playback can begin,
                          * but it may run out of data before playback finishes.
                          */
                         if (loadState.contains(MPMovieLoadState.Playable)) {
-                            overlayController.setLoadStateDisplayString("playable");
+//                            overlayController.setLoadStateDisplayString("playable");
+                            status.setText("Ready");
                         }
                         /*
                          * Enough data has been buffered for playback to
@@ -50,12 +54,14 @@ public class IOSShoutCastPlayer implements Player {
                          */
                         if (loadState.contains(MPMovieLoadState.PlaythroughOK)) {
                             // Add an overlay view on top of the movie view
-                            addOverlayView();
-                            overlayController.setLoadStateDisplayString("playthrough ok");
+//                            addOverlayView();
+//                            overlayController.setLoadStateDisplayString("playthrough ok");
+                            status.setText("Buffering");
                         }
                         /* The buffering of data has stalled. */
                         if (loadState.contains(MPMovieLoadState.Stalled)) {
-                            overlayController.setLoadStateDisplayString("stalled");
+//                            overlayController.setLoadStateDisplayString("stalled");
+                            status.setText("Stalled");
                         }
                     }
                 });
